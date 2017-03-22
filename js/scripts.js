c4b3d14165162c3f38125e3b89dda6eff786e50d
Krzysztof Barański <Krzysztof Baraski>
2017-03-23 00:12:08 +0100

    zadanie 8.3

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imiê');
+alert('Witaj, ' + name); /*komentarz do linii alert*/
+
+
+//dodatkowy komentarz
+/*jeszcze jeden komentarz*/
+console.log('Witaj, ' + name);
